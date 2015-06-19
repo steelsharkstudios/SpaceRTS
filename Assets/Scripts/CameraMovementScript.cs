6dc5f48a5d9a52af1cec020379e21774eb3a6ee4
Matt <Kainte@gmail.com>
2015-06-19 15:53:57 -0700

    Added Camera Stuff

--- Assets/Scripts/CameraMovementScript.cs
+++ Assets/Scripts/CameraMovementScript.cs
@@ -10,10 +10,17 @@ public class CameraMovementScript : MonoBehaviour {
 
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-
+		if (Input.GetButton("Up"))
+			transform.Translate(0, movementSpeed * Time.deltaTime, 0);
+		if (Input.GetButton("Left"))
+			transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
+		if (Input.GetButton("Right"))
+			transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
+		if (Input.GetButton("Down"))
+			transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
 	}
 }
