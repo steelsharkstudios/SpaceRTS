14f7ca89c4e75ec5f67c90954a4523005b476872
Matt <Kainte@gmail.com>
2015-06-19 23:31:12 -0700

    Units now have minimap representation Project organization

---
+++ Assets/Scripts/UnitProperties.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitProperties : MonoBehaviour {
+
+	public GameObject minimapIcon;
+
+	// Use this for initialization
+	void Start () {
+		
+		GameObject.Instantiate(minimapIcon, transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		
+	}
+}
