f402bda45aba116e68701fc6337afb6f5f072381
Matt <Kainte@gmail.com>
2015-06-19 22:22:38 -0700

    Added Minimap move script

---
+++ Assets/Scripts/Camera Scripts/CameraMiniMapMoveScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMiniMapMoveScript : MonoBehaviour {
+
+	public GameObject minimapCameraObject;
+	public GameObject gameCameraObject;
+
+	private Camera minimapCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+		if (minimapCameraObject != null)
+			minimapCamera = minimapCameraObject.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+
+			Vector3 mouseVector = new Vector3(Input.mousePosition.x,Input.mousePosition.y, 100.0f);
+			Vector3 worldPosition = minimapCamera.ScreenToWorldPoint(mouseVector);
+
+			worldPosition.y = gameCameraObject.transform.position.y;
+			gameCameraObject.transform.position = worldPosition;
+		}
+	}
+}
