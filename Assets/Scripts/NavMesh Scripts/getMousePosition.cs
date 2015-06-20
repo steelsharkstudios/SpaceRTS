4548d85fb4b77f00c42db32c9fa0d31c3b59aad3
Sean Powell <seanpowell54@gmail.com>
2015-06-20 14:53:18 +0200

    getMousePosition returns a vector3 of the mouse position NavMeshScript take the object it is attached to and moves it to the target position,
    Note:
    I am not going to be able to work as much as need in the next 2-3 days so Matt if you want to finish the script off that would be helpful, you need to just get the target to move to the mouseclick position then move the attached object too it.

---
+++ Assets/Scripts/NavMesh Scripts/getMousePosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getMousePosition : MonoBehaviour {
+	private float xMouse;
+	private float yMouse;
+	private float zMouse;
+
+	Vector3 MousePosition(){
+		xMouse = Input.mousePosition.x;
+		yMouse = Input.mousePosition.y;
+		zMouse = Input.mousePosition.z;
+		Vector3 pos = new Vector3 (xMouse, yMouse, zMouse);
+		return pos;
+	}
+}
