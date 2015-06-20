07e988116f359d7a5e0e90d3f2b49aa657969985
Matt <Kainte@gmail.com>
2015-06-20 13:33:34 -0700

    Readded prefabs

---
+++ Assets/Scripts/NavMesh Scripts/NewScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	public GameObject target;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		agent.SetDestination(target.transform.position);
+	}
+}
