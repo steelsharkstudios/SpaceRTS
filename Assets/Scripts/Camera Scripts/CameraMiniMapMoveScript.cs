78863158295ec30929d6831b0d212813ac0f9990
Matt <Kainte@gmail.com>
2015-06-19 23:11:56 -0700

    Added Minimap border Polished Minimap smoothing

--- Assets/Scripts/Camera Scripts/CameraMiniMapMoveScript.cs
+++ Assets/Scripts/Camera Scripts/CameraMiniMapMoveScript.cs
@@ -18,7 +18,12 @@ public class CameraMiniMapMoveScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButton(0)) {
+
+			// Returns if cursor isn't over the minimap
+			Vector3 viewportPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+			if (!minimapCamera.rect.Contains(viewportPosition))
+				return;
 
 			Vector3 mouseVector = new Vector3(Input.mousePosition.x,Input.mousePosition.y, 100.0f);
 			Vector3 worldPosition = minimapCamera.ScreenToWorldPoint(mouseVector);
---
+++ Assets/Scripts/Camera Scripts/CameraMinimapGUIScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMinimapGUIScript : MonoBehaviour {  
+
+	public GameObject minimapCameraObject;
+
+	public GUISkin minimapSkin;
+
+	private Camera minimapCamera;
+	
+	// Use this for initialization
+	void Start () {
+		
+		if (minimapCameraObject != null)
+			minimapCamera = minimapCameraObject.GetComponent<Camera>();
+	}
+	
+	void OnGUI() {
+
+		if (minimapSkin)
+			GUI.skin = minimapSkin;
+
+		GUI.Box(new Rect(minimapCamera.pixelRect.x, (Screen.height - minimapCamera.pixelRect.yMax), 
+		             minimapCamera.pixelWidth, minimapCamera.pixelHeight), "");
+	}
+}
