f7d6ea809fa108fa77a1e8e78250e04472e8cf85
Sean Powell <seanpowell54@gmail.com>
2015-06-20 22:18:36 +0200

    Added a resorce counter ui to the camera
    ResorceCounter Methods are
    getUranium() returns the uranium left
    getTitanium() returns the titanium left
    getCurrentUnitCount() rerurns the currnet number of units
    changeUranium(int) changes the ammount of uranium by the int given, accepts negetive numbers
    changeTitanium(int) changes the ammount of titanium by the int given, accepts negetive numbers
    unityCreated() adds one to the current unit count
    unitDestroyed() removes one from the the current unit count

---
+++ Assets/Scripts/Gui Scripts/ResorceCounter.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ResorceCounter : MonoBehaviour {
+	public Text Uranium;
+	public Text Titanium;
+	public Text CurrentUnit;
+	public Text MaxUnit;
+
+	private int UraniumCount;
+	private int TitaniumCount;
+	private int CurrentUnitCount;
+	private int MaxUnitsCount;
+
+	// Use this for initialization
+	void Start () {
+		UraniumCount = 250;
+		TitaniumCount = 500;
+		CurrentUnitCount = 0;
+		MaxUnitsCount = 200;
+
+		Uranium.text = UraniumCount + "";
+		Titanium.text = TitaniumCount + "";
+		CurrentUnit.text  = CurrentUnitCount + "";
+		MaxUnit.text  = MaxUnitsCount + "";
+	}
+	
+	public int getUranium(){
+		return UraniumCount;
+	}
+
+	public int getTitanium(){
+		return TitaniumCount;
+	}
+
+	public int getCurrentUnitCount(){
+		return CurrentUnitCount;
+	}
+
+	public void changeUranium(int x){
+		UraniumCount = UraniumCount + x;
+		Uranium.text = UraniumCount + "";
+	}
+
+	public void changeTitanium(int x){
+		TitaniumCount = UraniumCount + x;
+		Titanium.text = TitaniumCount + "";
+	}
+
+	public void unitCreated(){
+		CurrentUnitCount++;
+		CurrentUnit.text = CurrentUnitCount + "";
+	}
+
+	public void unitDestroyed(){
+		CurrentUnitCount--;
+		CurrentUnit.text = CurrentUnitCount + "";
+	}
+
+}
