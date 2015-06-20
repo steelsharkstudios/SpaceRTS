97ba2411d8511298da701663ac570fba4e2850ce
Matt <Kainte@gmail.com>
2015-06-20 13:03:19 -0700

    Updated Scripts

--- Assets/Scripts/NavMesh Scripts/GetMousePosition.cs
+++ Assets/Scripts/NavMesh Scripts/GetMousePosition.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 
 public class getMousePosition : MonoBehaviour {
+
 	private float xMouse;
 	private float yMouse;
 	private float zMouse;
 
-	Vector3 MousePosition(){
+	Vector3 MousePosition() {
+
 		xMouse = Input.mousePosition.x;
 		yMouse = Input.mousePosition.y;
 		zMouse = Input.mousePosition.z;
-		Vector3 pos = new Vector3 (xMouse, yMouse, zMouse);
+
+		Vector3 pos = new Vector3(xMouse, yMouse, zMouse);
+
 		return pos;
 	}
 }
--- Assets/Scripts/UnitProperties.cs
+++ Assets/Scripts/UnitProperties.cs
@@ -13,7 +13,7 @@ public class UnitProperties : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-			
-		
+
+
 	}
 }
