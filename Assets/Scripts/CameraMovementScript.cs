0c6dd77b6903edf7fed3d720a7633f3df02d2c4d
Matt <Kainte@gmail.com>
2015-06-19 21:18:19 -0700

    Finishing up Camera Scripts and Input

--- Assets/Scripts/CameraMovementScript.cs
+++ Assets/Scripts/CameraMovementScript.cs
@@ -5,22 +5,16 @@ public class CameraMovementScript : MonoBehaviour {
 
 	public float movementSpeed = 20;
 
-	// Use this for initialization
-	void Start () {
-
-
-	}
-
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetButton("Up"))
-			transform.Translate(0, movementSpeed * Time.deltaTime, 0);
+			transform.Translate(0, 0, movementSpeed * Time.deltaTime);
 		if (Input.GetButton("Left"))
 			transform.Translate(-movementSpeed * Time.deltaTime, 0, 0);
 		if (Input.GetButton("Right"))
 			transform.Translate(movementSpeed * Time.deltaTime, 0, 0);
 		if (Input.GetButton("Down"))
-			transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
+			transform.Translate(0, 0, -movementSpeed * Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/CameraRotateScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotateScript : MonoBehaviour {
+
+	public float rotateTime = 0.3f;
+	public float degreeRotation = 90.0f;
+
+	private bool rotationInProgress = false;
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Don't allow input while rotation is in progress
+		if (rotationInProgress)
+			return;
+
+		// Controls
+		if (Input.GetButtonDown("Rotate Left"))
+			StartCoroutine("Rotate", true);
+		if (Input.GetButtonDown("Rotate Right"))
+			StartCoroutine("Rotate", false);
+	}
+
+	// Rotates camera based on public variables
+	IEnumerator Rotate(bool turningLeft) {
+
+		rotationInProgress = true;
+
+		Quaternion startRotation = transform.rotation;
+		Quaternion endRotation = transform.rotation ;
+
+		if (turningLeft)
+			endRotation *= Quaternion.Euler(0, degreeRotation, 0);
+		else
+			endRotation *= Quaternion.Euler(0, -degreeRotation, 0);
+
+		float time = 0f;
+		float rate = 1.0f / rotateTime;
+
+		while (time < 1.0f) {
+			
+			time += Time.deltaTime * rate;
+			transform.rotation = Quaternion.Slerp(startRotation, endRotation, time);
+			yield return null;
+		}
+
+		rotationInProgress = false;
+	}
+}
